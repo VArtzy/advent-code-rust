16b9d0c31c3e087e64fac1b4b9dcab1a54fdf936
VArtz <farrelnikoson@gmail.com>
2024-12-03 00:14:53 +0700

    Day 1: Historian Hysteria

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
     let mut left = vec![3, 4, 2, 1, 3, 3];
     let mut right = vec![4, 3, 5, 3, 9, 3];
+    let mut idx = 0;
+    let mut result = vec![];
+    let mut sum = 0;
     left.sort();
     right.sort();
-    for num in left.iter() {
-        println!("{:?}", num + right.iter().next().unwrap_or_default());
+    while idx < left.len() {
+       let r: i32 = right[idx] - left[idx];
+       result.push(r.abs());
+       idx += 1;
     }
+    for num in result {
+        sum += num;
+    }
+    println!("{}", sum);
 }
