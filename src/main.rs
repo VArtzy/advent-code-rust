4de0843d126c35d537edaf05b324c836df475e67
VArtz <farrelnikoson@gmail.com>
2024-12-04 14:20:15 +0700

    Day 2: Red-Nosed Reports pt 1 skill and memory issue

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,26 @@ use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("src/input.txt").expect("should have been able to read the file");
+    let mut count = 0;
     for line in contents.lines() {
         let vec: Vec<i32> = line.split_whitespace().map(|s| s.parse().expect("parse failed")).collect();
         let mut idx = 1;
-        let mut dec = false;
-        let mut inc = false;
+        let mut dec = true;
+        let mut inc = true;
         let mut safe = true;
         while idx < vec.len() {
            if vec[idx - 1] < vec[idx] || vec[idx - 1] - vec[idx] > 3 {
+               dec = false;
                 break;
            }
-           dec = true;
            idx += 1;
         }
         idx = 1;
         while idx < vec.len() {
            if vec[idx - 1] > vec[idx] || vec[idx] - vec[idx - 1] > 3 {
+               inc = false;
                 break;
            }
-           inc = true;
            idx += 1;
         }
         idx = 1;
@@ -31,6 +32,9 @@ fn main() {
            }
            idx += 1;
         }
-        println!("inc:{}, dec:{}, safe: {}", inc, dec, safe);
+        if (inc || dec) && safe {
+            count += 1;
+        }
     }
+    println!("{}", count);
 }
