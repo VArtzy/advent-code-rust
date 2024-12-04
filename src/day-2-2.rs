e645826e287819fa83dabfc1292f62ffa035da74
VArtz <farrelnikoson@gmail.com>
2024-12-04 20:18:08 +0700

    Day 3: Mull It Over pt 1 Im the best non regex programmer

---
+++ src/day-2-2.rs
@@ -0,0 +1,55 @@
+
+fn main() {
+    let contents = fs::read_to_string("src/input.txt").expect("should have been able to read the file");
+    let mut count = 0;
+    for line in contents.lines() {
+        let mut vec: Vec<i32> = line.split_whitespace().map(|s| s.parse().expect("parse failed")).collect();
+        let mut idx = 1;
+        let mut dec = true;
+        let mut tolerate = true;
+        let mut inc = true;
+        let mut safe = true;
+        while idx < vec.len() - 1 {
+           if vec[idx - 1] < vec[idx] || vec[idx - 1] - vec[idx] > 3 {
+               if tolerate && vec[idx - 1] < vec[idx + 1] && vec[idx + 1] - vec[idx - 1] < 3 {
+                   vec.remove(idx);
+                    tolerate = false;
+                    continue;
+               }
+               dec = false;
+                break;
+           }
+           idx += 1;
+        }
+        idx = 1;
+        while idx < vec.len() - 1 {
+           if vec[idx - 1] > vec[idx] || vec[idx] - vec[idx - 1] > 3 {
+               if tolerate && vec[idx - 1] > vec[idx + 1] && vec[idx - 1] - vec[idx + 1] < 3 {
+                   vec.remove(idx);
+                    tolerate = false;
+                    continue;
+               }
+               inc = false;
+                break;
+           }
+           idx += 1;
+        }
+        idx = 1;
+        while idx < vec.len() - 1 {
+           if vec[idx - 1] == vec[idx] {
+               if tolerate && vec[idx - 1] != vec[idx + 1] {
+                   vec.remove(idx);
+                    tolerate = false;
+                    continue;
+               }
+                safe = false;
+                break;
+           }
+           idx += 1;
+        }
+        if (inc || dec) && safe {
+            count += 1;
+        }
+    }
+    println!("{}", count);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,15 @@ use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("src/input.txt").expect("should have been able to read the file");
-    let mut count = 0;
-    for line in contents.lines() {
-        let mut vec: Vec<i32> = line.split_whitespace().map(|s| s.parse().expect("parse failed")).collect();
-        let mut idx = 1;
-        let mut dec = true;
-        let mut tolerate = true;
-        let mut inc = true;
-        let mut safe = true;
-        while idx < vec.len() - 1 {
-           if vec[idx - 1] < vec[idx] || vec[idx - 1] - vec[idx] > 3 {
-               if tolerate && vec[idx - 1] < vec[idx + 1] && vec[idx + 1] - vec[idx - 1] < 3 {
-                   vec.remove(idx);
-                    tolerate = false;
-                    continue;
-               }
-               dec = false;
-                break;
-           }
-           idx += 1;
-        }
-        idx = 1;
-        while idx < vec.len() - 1 {
-           if vec[idx - 1] > vec[idx] || vec[idx] - vec[idx - 1] > 3 {
-               if tolerate && vec[idx - 1] > vec[idx + 1] && vec[idx - 1] - vec[idx + 1] < 3 {
-                   vec.remove(idx);
-                    tolerate = false;
-                    continue;
-               }
-               inc = false;
-                break;
-           }
-           idx += 1;
-        }
-        idx = 1;
-        while idx < vec.len() - 1 {
-           if vec[idx - 1] == vec[idx] {
-               if tolerate && vec[idx - 1] != vec[idx + 1] {
-                   vec.remove(idx);
-                    tolerate = false;
-                    continue;
-               }
-                safe = false;
-                break;
-           }
-           idx += 1;
-        }
-        if (inc || dec) && safe {
-            count += 1;
-            println!("Safe: {:?}", vec);
-        } else {
-            println!("Unsafe: {:?}", vec);
+    let muls: Vec<&str> =  contents.split("mul(").collect();
+    let mut sum = 0;
+    for mul in muls {
+        let separator: Vec<&str> = mul.split(|c| c == ')' || c == ',').collect();
+        if let Some(x) = separator[0].parse::<i32>().ok() {
+            if let Some(y) = separator[1].parse::<i32>().ok() {
+                sum += x * y;
+            }
         }
     }
-    println!("{}", count);
+    println!("{}", sum);
 }
