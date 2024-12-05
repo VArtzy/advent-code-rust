07a8aac3fec9d1216e8d7150d5809c671a95cbe0
VArtz <farrelnikoson@gmail.com>
2024-12-05 12:07:17 +0700

    Day 3: Mull It Over pt 2 Im the best anti-regex programmer

---
+++ src/day-3-1.rs
@@ -0,0 +1,17 @@
+
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("src/input.txt").expect("should have been able to read the file");
+    let muls: Vec<&str> =  contents.split("mul(").collect();
+    let mut sum = 0;
+    for mul in muls {
+        let separator: Vec<&str> = mul.split(|c| c == ')' || c == ',').collect();
+        if let Some(x) = separator[0].parse::<i32>().ok() {
+            if let Some(y) = separator[1].parse::<i32>().ok() {
+                sum += x * y;
+            }
+        }
+    }
+    println!("{}", sum);
+}
